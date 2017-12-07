be9780aec04c936b305edcea89c7158f3a68fa70
selesdepselesnul <selesdepselesnul@gmail.com>
2017-12-08 06:06:55 +0700

    add filter function

--- src/i_b_w_y/core.clj
+++ src/i_b_w_y/core.clj
@@ -2,14 +2,32 @@
   (:gen-class)
   (:require [hawk.core :as hawk]))
 
-(hawk/watch! [{:paths ["/home/morrisseymarr/watched"]
-               :handler (fn [ctx e]
-                          (println "event: " e)
-                          (println "context: " ctx)
-                          ctx)}])
+(defn when-file-changed [ctx e]
+  (println (:kind e))
+  (println (.getName (:file e)))
+  ctx)
+
+(def dir-being-watched "/home/morrisseymarr/watched")
+
+(defn watch [dir]
+  (hawk/watch! [{:paths [dir]
+                 :handler when-file-changed}]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn filter-files-with-pattern [dir pattern]
+  (let [f (clojure.java.io/file dir)]
+    (->> (file-seq f)
+         (filter #(re-matches pattern (.getName %))))))
+
+(def pattern-to-filter #"^mantab.*$")
+
+(filter-files-with-pattern dir-being-watched pattern-to-filter)
+
+
+
+
 
