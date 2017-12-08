cf1d05e66603ffa249df17c2dd64c8549e2a73e5
selesdepselesnul <selesdepselesnul@gmail.com>
2017-12-08 20:19:51 +0700

    add function for delete files

--- src/i_b_w_y/core.clj
+++ src/i_b_w_y/core.clj
@@ -1,33 +1,34 @@
 (ns i-b-w-y.core
   (:gen-class)
-  (:require [hawk.core :as hawk]))
+  (:require [hawk.core :as hawk]
+            [clojure.java.io :as io]))
 
-(defn when-file-changed [ctx e]
-  (println (:kind e))
-  (println (.getName (:file e)))
-  ctx)
+
 
 (def dir-being-watched "/home/morrisseymarr/watched")
+
+(defn filter-files-with-pattern [dir pattern except-for]
+  (let [f (clojure.java.io/file dir)]
+    (->> (file-seq f)
+         (filter 
+          #(let [f-name (.getName %)]
+             (and
+              (re-matches pattern f-name)
+              (not= except-for f-name)))))))
+
+(def pattern-to-filter #"^mantab.*$")
+
+(defn delete-files [dir pattern except]
+  (doseq [x (filter-files-with-pattern dir pattern except)]
+    (io/delete-file x)))
+
+(defn when-file-changed [ctx e]
+  (delete-files dir-being-watched pattern-to-filter "mantab.jpg")
+  ctx)
 
 (defn watch [dir]
   (hawk/watch! [{:paths [dir]
                  :handler when-file-changed}]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
-(defn filter-files-with-pattern [dir pattern]
-  (let [f (clojure.java.io/file dir)]
-    (->> (file-seq f)
-         (filter #(re-matches pattern (.getName %))))))
-
-(def pattern-to-filter #"^mantab.*$")
-
-(filter-files-with-pattern dir-being-watched pattern-to-filter)
-
-
-
-
+(watch dir-being-watched)
 
